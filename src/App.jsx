8c7245ce9b0f2791243c66b3dbcbea729734f4af
Leonardo-SantosDev <leonardol.santos2005@gmail.com>
2025-04-29 12:10:18 -0300

    Basename para usar o github pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import styles from './App.module.css';
 import './global.css';
 
-
 import { Login } from './pages/auth/Login';
 import { Cadastro } from '../src/pages/auth/Cadastro';
-import  Home  from '../src/pages/home/Home';
+import Home from '../src/pages/home/Home';
 import Contato from './pages/home/Contato';
 import Sobre from './pages/home/Sobre';
 import Funcionalidades from './pages/home/Funcionalidades';
@@ -13,10 +12,10 @@ import Funcionalidades from './pages/home/Funcionalidades';
 function App() {
   return (
     <div className={styles.App}>
-      <Router>
-        <Routes basename="/CryptoNexus-Front2.0">
-          <Route path="/" element={<Home/>} />
-          <Route path="/login" element={<Login/>} />
+      <Router basename="/CryptoNexus-Front2.0">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/cadastro" element={<Cadastro />} />
           <Route path="/contato" element={<Contato />} />
           <Route path="/sobre" element={<Sobre />} />
