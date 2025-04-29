e9629543ad8eda792746cdf82408f18d7d91b16a
Leonardo-SantosDev <leonardol.santos2005@gmail.com>
2025-04-29 11:40:00 -0300

    Configuração para deploy no GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/CryptoNexus-Front2.0/',
+  base: '/CryptoNexus-Front2.0/', 
   plugins: [react()],
 })
